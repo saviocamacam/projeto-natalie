7132512fd6f784d88c4ab98f69ef5a5f702b1fe3
saviocamacam <saviocamacam@alunos.utfpr.edu.br>
2016-04-02 18:32:25 -0300

    correcao e adição da pasta dev

---
+++ projeto_dev/lib.h
@@ -0,0 +1,8 @@
+#ifndef LIB_H_
+#define LIB_H_
+#include <stdio.h>
+#include <stdlib.h>
+#define ItemType int
+
+
+#endif
---
+++ projeto_dev/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	char **string;
+}lista_string;
+
+int contaLinha(FILE *fp) {
+	int nroLinhas = 0;
+	while(!feof(fp)) {
+		nroLinhas++;
+	}
+	return nroLinhas;
+}
+
+int inserirLista(lista_string *l, char *s, int index) {
+	int tam = strlen(s);
+	l->string[index] = (char*) calloc (tam,sizeof(char));
+	strcpy(l->string[index],p);
+	return 0;
+}
+
+lista_string * criaLista(int cap) {
+	lista_string *eLS = (lista_string*) calloc (1,sizeof(lista_string));
+	eLS->string = (char**) calloc (cap,sizeof(char**));
+	return eLS;
+}
+
+lista_string importar(FILE * fp, lista_string *l) {
+	int i=0, j=0, t;
+	char c;
+	char copia[T];	
+	while(!feof(fp)) {
+		fgets(copia,100,fp);
+		t = strlen(copia);
+		for (i=0 ; i<t ; i++) {
+			if (copia[i] == '\n') {
+				copia[i] = '\0';
+			}
+		}
+		inserirLista(l,copia, j);
+		j++;
+	}
+	fclose(fp);
+	remove_indice(l,(l->qtd-1));
+	return l;
+}
+
+void imprime (lista_string *l) {
+	int i;
+	for (i=0 ; i< l->qtd ; i++) {
+		printf ("STRING (%d):  %s\n", i, l->string[i]);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	int cap;
+	FILE *fp;
+	fp = fopen("modelo1.txt","r");
+	if (!fp) {
+		printf ("Erro na abertura do arquivo!\n");
+		exit(1);
+	}
+	fp = fopen("modelo1.txt","r");
+	cap = contaLinha(fp);
+	lista_string * lista = criaLista(cap);
+	lista = importar(lista);
+	imprime(lista);
+	return 0;
+}
